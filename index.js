efa3f0ae831c193089959ef0c70b7e8e704e2fea
Andrew Nelson <andrewnelson@earthlink.net>
2015-09-10 22:19:17 -0700

    more tests, more uniform tests, added gulp

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 'use strict';
 
-function ConResp(r) {
+function CompRest(r) {
   this.resp = r;
 }
 
-ConResp.prototype.text = function(str, status) {
-  this.resp.writeHead((status? status : 200), {'Content-Type': 'text/plain'})
-  // resp.write(str);
-  // resp.end();
-}
+CompRest.prototype.text = function(str, status) {
+  this.resp.writeHead((status? status : 200), {'Content-Type': 'text/plain'});
+  this.resp.write(str);
+  this.resp.end();
+};
+
+CompRest.prototype.json = function(obj, status) {
+  this.resp.writeHead((status? status : 200), {'Content-Type': 'application/json'});
+  this.resp.write(obj);
+  this.resp.end();
+};
+
+CompRest.prototype.pipe = function(stream, status) {
+  this.resp.writeHead((status? status : 200), {'Content-Type': 'text/plain'});
+  stream.pipe(this.resp);
+};
 
 var framework = module.exports = exports = function(rsp) {
-  return new ConResp(rsp);
+  return new CompRest(rsp);
 };
